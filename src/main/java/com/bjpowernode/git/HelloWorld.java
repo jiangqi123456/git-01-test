c270b181d0282d020ccb46a663cba50d9c3d255d
jiangqi123456 <405168828@qq.com>
2022-10-18 17:10:55 +0800

    init  git-01-test

---
+++ src/main/java/com/bjpowernode/git/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.git;
+
+/**
+ * Author:动力节点
+ * 2022/10/18
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("nijo");
+    }
+}
