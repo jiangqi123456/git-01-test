aee9b793207d7a7f12623793c53fd70a3320e0be
jiangqi123456 <405168828@qq.com>
2022-10-19 13:44:58 +0800

    eidt helloworld by dev

--- src/main/java/com/bjpowernode/git/HelloWorld.java
+++ src/main/java/com/bjpowernode/git/HelloWorld.java
@@ -13,5 +13,7 @@ public class HelloWorld {
         System.out.println("nimen");
         System.out.println("nimen zaibuzai123");
         System.out.println("123456");
+        System.out.println("12");
+        System.out.println("1111");
     }
 }
